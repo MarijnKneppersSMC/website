2eace4e5450fa02594724d68620d3dc5c04238ed
Marijn Kneppers <95086271+MarijnKneppersSMC@users.noreply.github.com>
2022-01-31 16:32:37 +0000

    Marijn: simplified code to embed html files

--- scripts/embedHTML.js
+++ scripts/embedHTML.js
@@ -1,31 +1,28 @@
 function embedHTML() {
 
-	// get a list of all HTML elements
-	let elements = document.getElementsByTagName("*");
+	// get a list of all HTML elements with the embed-html attribute
+	let elements = document.querySelectorAll('[embed-html]')
 
-	// start a loop that kiios over all html elements
+	// start a loop that loops over all html elements
 	for (let i = 0; i < elements.length; i++) {
 
 		// get the element at the current point in the loop
 		let element = elements[i];
 
-		// check if the element has the "embed-html" attribute and if so, add it to the inner html of the div
-		if (element.hasAttribute("embed-html")) {
+		// load page referred to in the attribute and add it to the inner html of the element
+		let file = element.getAttribute("embed-html");
 
-			let file = element.getAttribute("embed-html");
-
-			let xhttp = new XMLHttpRequest();
-			xhttp.onreadystatechange = function () {
-				if (this.readyState == 4) {
-					if (this.status == 200) { element.innerHTML = this.responseText; }
-					if (this.status == 404) { element.innerHTML = "Page not found."; console.error(`${file} could not be found`) }
-					element.removeAttribute("embed-html");
-					embedHTML();
-				}
+		let xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function () {
+			if (this.readyState == 4) {
+				if (this.status == 200) { element.innerHTML = this.responseText; }
+				if (this.status == 404) { element.innerHTML = "Page not found."; console.error(`${file} could not be found`) }
+				element.removeAttribute("embed-html");
+				embedHTML();
 			}
-			xhttp.open("GET", file, true);
-			xhttp.send();
-			return;
 		}
+		xhttp.open("GET", file, true);
+		xhttp.send();
+		return;
 	}
 }
