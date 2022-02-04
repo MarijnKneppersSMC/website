35c2899e49f441b2b9c76d2e42a2fde598c745cf
Marijn Kneppers <95086271+MarijnKneppersSMC@users.noreply.github.com>
2022-02-04 13:47:25 +0000

    Marijn: layed basics for modal

---
+++ scripts/modal.js
@@ -0,0 +1,18 @@
+function toggleModal()
+{
+	let modal = document.getElementById("modal");
+
+	if(modal.classList.contains("open"))
+	{
+		modal.classList.remove("open");
+	}
+	else
+	{
+		modal.classList.add("open");
+	}
+}
+
+function setModalContent(id)
+{
+	
+}
