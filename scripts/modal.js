3d9eaddddd8598314815976a9b7e542601e20524
Marijn Kneppers <95086271+MarijnKneppersSMC@users.noreply.github.com>
2022-02-04 14:47:06 +0000

    Marijn: finished modal draft Tijmen: started on styling

--- scripts/modal.js
+++ scripts/modal.js
@@ -2,17 +2,25 @@ function toggleModal()
 {
 	let modal = document.getElementById("modal");
 
+	let glass = document.getElementById("glass-pane")
+
 	if(modal.classList.contains("open"))
 	{
 		modal.classList.remove("open");
+		glass.classList.remove("shown");
 	}
 	else
 	{
 		modal.classList.add("open");
+		glass.classList.add("shown");
 	}
 }
 
 function setModalContent(id)
 {
-	
+	let dialogue = document.getElementById("modal-dialogue");
+
+	let data = document.getElementById(id);
+
+	dialogue.innerHTML = data.innerHTML;
 }
